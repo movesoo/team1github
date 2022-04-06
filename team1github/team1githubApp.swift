a7562fb163e69c02b0c554a2f298a158aeed8af4
movesoo <103127692+movesoo@users.noreply.github.com>
2022-04-07 08:56:55 +0900

    Initial commit

---
+++ team1github/team1githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  team1githubApp.swift
+//  team1github
+//
+//  Created by 박성수 on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct team1githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
